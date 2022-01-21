64715b6647e4e3ac8563a1f180e34430c56f3b43
Alfonso Gallardo Rodríguez <gallardo.roalf20@triana.salesianos.edu>
2022-01-21 12:42:04 +0100

    Añadiendo bottom navigation bar

---
+++ flutter_bottom_navigation_bar/lib/pages/personajes_pages.dart
@@ -0,0 +1,110 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bottom_navigation_bar/models/personajes_response.dart';
+import 'package:http/http.dart' as http;
+
+class PersonajesPages extends StatefulWidget {
+  const PersonajesPages({Key? key}) : super(key: key);
+
+  @override
+  _PersonajesPagesState createState() => _PersonajesPagesState();
+}
+
+
+class _PersonajesPagesState extends State<PersonajesPages> {
+  late Future<List<Personajes>> items;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Star Wars',
+            style: TextStyle(fontFamily: 'StarJedi'),
+          ),
+        ),
+        body: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Stack(children: <Widget>[
+              Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                          "assets/images/fondo_estrellas.gif",
+                        ),
+                        fit: BoxFit.cover)),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          'Planetas',
+                          style: TextStyle(
+                              fontFamily: 'StarJedi',
+                              color: Colors.white,
+                              fontSize: 20),
+                        ),
+                      ),
+                      Card(
+                        color: Colors.transparent,
+                        margin: const EdgeInsets.only(top: 20),
+                        child: SizedBox(
+                          height: 200,
+                          child: FutureBuilder<List<Personajes>>(
+                              future: items,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData) {
+                                  return _personajesList(snapshot.data!);
+                                } else if (snapshot.hasError) {
+                                  return Text('${snapshot.error}');
+                                }
+                                return const CircularProgressIndicator();
+                              }),
+                        ),
+                      ),
+                    ]),
+              )
+            ])));
+  }
+
+  Future<List<Personajes>> fetchPersonajes() async {
+    final response =
+        await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+    if (response.statusCode == 200) {
+      return PersonajesResponse.fromJson(jsonDecode(response.body)).results;
+    } else {
+      throw Exception('Failed to load persons');
+    }
+  }
+
+  Widget _personajesList(List<Personajes> personajesList) {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: personajesList.length,
+      itemBuilder: (context, index) {
+        return _personajesItem(personajesList.elementAt(index), index);
+      },
+    );
+  }
+
+  Widget _personajesItem(Personajes personajes, int index) {
+    return Card(
+      color: Colors.transparent,
+      child: Column(
+        children: <Widget>[
+          Text(personajes.name),
+          Image.network(
+            'https://rickandmortyapi.com/api/character/avatar/${index + 1}.jpeg',
+            width: 120,
+          )
+        ],
+      ),
+    );
+  }
+}
