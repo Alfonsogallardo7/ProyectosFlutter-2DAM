35b0bb5a87a4992016dd3cd52359b3eaab93a363
Alfonso Gallardo Rodríguez <alfonso@MBP-de-Alfonso.home>
2022-02-01 09:00:59 +0100

    Empezando a añadir los detalles de peliculas

--- popcorn_home_page/lib/pages/movie_page.dart
+++ popcorn_home_page/lib/pages/movie_page.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'dart:ffi';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:popcorn_home_page/models/movie_response.dart';
 
 class MoviePage extends StatefulWidget {
   const MoviePage({Key? key}) : super(key: key);
@@ -37,11 +39,11 @@ class _MoviePageState extends State<MoviePage> {
           )])),
     ));
   }
-  Future<List<MoviePage>> fetchUpFilms() async {
+  Future<List<MoviePage>> fetchMovie() async {
     final response = await http.get(Uri.parse(
         'https://api.themoviedb.org/3/movie/upcoming?api_key=d64edfd74101230753aa7b2393ffe8df&language=en-US&page=1'));
     if (response.statusCode == 200) {
-      return UpComingResponse.fromJson(jsonDecode(response.body)).results;
+      return MovieResponse.fromJson(jsonDecode(response.body);
     } else {
       throw Exception('Failed to load films');
     }
