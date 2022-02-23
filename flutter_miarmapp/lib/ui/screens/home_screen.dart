5618f790caf3c27e1aef147f83be9759ff49a808
Alfonso Gallardo Rodríguez <alfonso@MacBook-Pro.home>
2022-02-24 00:28:36 +0100

    Sigo añadiendo

--- flutter_miarmapp/lib/ui/screens/home_screen.dart
+++ flutter_miarmapp/lib/ui/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_miarmapp/models/posts_response.dart';
+import 'package:flutter_miarmapp/repository/post_repository/post_repository.dart';
+import 'package:flutter_miarmapp/repository/post_repository/post_respository_impl.dart';
 import 'package:flutter_miarmapp/utils/color_style.dart';
 import 'package:flutter_miarmapp/ui/widgets/home_app_bar.dart';
 import 'package:flutter_miarmapp/ui/widgets/like_animations.dart';
@@ -12,12 +16,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late PostRepository movieRepository;
   bool isLikeAnimation = false;
+
+  @override
+  void initState() {
+    super.initState();
+    movieRepository = PostRespositoryImpl();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
             appBar: const HomeAppBar(),
-            body: Container(
+            body: /*Container(
       padding: EdgeInsets.symmetric(vertical: 10),
       child: Column(
         children: [
@@ -224,6 +241,83 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       
-    ));
+    )*/);
+  }
+
+  Widget _createPopularView(BuildContext context, List<Post> post) {
+    final contentHeight = 4.0 * (MediaQuery.of(context).size.width / 2.4) / 3;
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.only(left: 20.0, right: 16.0),
+          height: 48.0,
+          child: Row(
+            children: [
+              const Expanded(
+                flex: 1,
+                child: Text(
+                  'Popular',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 16.0,
+                    fontFamily: 'Muli',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Icon(Icons.arrow_forward, color: Colors.red),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: contentHeight,
+          child: ListView.separated(
+            itemBuilder: (BuildContext context, int index) {
+              return _createPopularView(context, post[index]);
+            },
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            scrollDirection: Axis.horizontal,
+            separatorBuilder: (context, index) => const VerticalDivider(
+              color: Colors.transparent,
+              width: 6.0,
+            ),
+            itemCount: post.length,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _createPostViewItem(BuildContext context, Post post) {
+    final width = MediaQuery.of(context).size.width / 2.6;
+    return Container(
+      width: width,
+      height: double.infinity,
+      padding: const EdgeInsets.only(bottom: 20.0),
+      child: Card(
+        elevation: 10.0,
+        borderOnForeground: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: SizedBox(
+          width: width,
+          height: double.infinity,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: CachedNetworkImage(
+              placeholder: (context, url) => const Center(
+                child: CircularProgressIndicator(),
+              ),
+              imageUrl: 'https://image.tmdb.org/t/p/w500${post.fichero}',
+              width: width,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
