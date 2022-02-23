5b0b8eff44b48890ac6a3ddbf6a6bc96604d0dfa
Alfonso Gallardo Rodríguez <alfonso@MacBook-Pro.home>
2022-02-24 00:27:47 +0100

    Empezando a meter la nueva estructura de paquetes

--- flutter_miarmapp/lib/repository/post_repository/post_respository_impl.dart
+++ flutter_miarmapp/lib/repository/post_repository/post_respository_impl.dart
@@ -7,10 +7,13 @@ import 'package:http/http.dart';
 class PostRespositoryImpl extends PostRepository {
   final Client _client = Client();
   @override
-  Future<List<Post>> fetchPosts(String type) { final response = await _client.get(Uri.parse('https://api.themoviedb.org/3/movie/'));
+  Future<List<Post>> fetchPosts(String type) async {
+    final response =
+        await _client.get(Uri.parse('https://api.themoviedb.org/3/movie/'));
     if (response.statusCode == 200) {
       return PostsResponse.fromJson(json.decode(response.body)).content;
     } else {
       throw Exception('Fail to load movies');
     }
+  }
 }
